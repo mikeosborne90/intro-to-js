3122b57a28c91e43f2b3e9007b3722e99491ce02
mikeosborne90 <mikeosborne1990@gmail.com>
2019-08-12 21:00:32 -0700

    Added a few functions.

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+function myFunk() 
+{
+    let question = prompt("Tell me your name.");
+    console.log("Your name is " + question);
+}
+
+function myHobbies() 
+{
+    console.log("Baseball");
+    console.log("Fishing");
+    console.log("Coding");
+    console.log("Hiking");
+}
+
+function BreakItUp()
+{
+    const QUESTION = prompt("")
+}
+
+function grabElementAndSpitOutValue()
+{
+    // get element from index
+
+    const MAIN_DIV = document.getElementById('mainDiv');
+    // write out in html
+
+    MAIN_DIV.innerHTML = "Hey, I am here!";
+    // execute
+
+    console.log("This hit.");
+
+}
