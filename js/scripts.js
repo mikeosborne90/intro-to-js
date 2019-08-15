f2f67f20864c3427e71f89b6fdf257c303536b0d
mikeosborne90 <mikeosborne1990@gmail.com>
2019-08-14 20:16:30 -0700

    Finished adding prompts and alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
-var firstName = "Michael";
-var age = 29;
-// old way
-console.log("My name is " + firstName + " and my age is " + age);
-// better way
-console.log(`My name is ${firstName} and my age is ${age}`);
+// var firstName = "Michael";
+// var age = 29;
+// // old way
+// console.log("My name is " + firstName + " and my age is " + age);
+// // better way
+// console.log(`My name is ${firstName} and my age is ${age}`);
 
-alert(`Hello, I am ${firstName}`);
+// alert(`Hello, I am ${firstName}`);
 
-const birthday = "02/19/1990";
-let fixDate = new Date(birthday);
-console.log(fixDate);
+// const birthday = "02/19/1990";
+// let fixDate = new Date(birthday);
+// console.log(fixDate);
 
-let hello = new String("Hello There");
-alert(hello);
+// let hello = new String("Hello There");
+// alert(hello);
 
+let firstName = prompt("What is your First Name?");
+let lastName = prompt("What is your Last Name?");
+
+console.log(`${firstName} ${lastName}`);
+
+alert(`You entered ${lastName} as your last name!`);
+
+let birthday = prompt("What is your birthday?");
+let correctBirthday = confirm(`${birthday}, Is this correct?`);
+alert(`${birthday}`);
