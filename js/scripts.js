a91afd8f9c85eafe12f1a92cae8108f332772219
mikeosborne90 <mikeosborne1990@gmail.com>
2019-09-04 12:17:00 -0700

    Finished day7 takehome

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,35 @@
-function printOneHundred()
+let title = prompt("Title?");
+
+titleH1 = document.getElementById("title");
+titleH1.innerText = title;
+
+let activities = prompt("What did you do today?");
+
+sentencesP = document.getElementById("sentences");
+sentencesP.innerText = activities;
+
+function isValidColor(strColor) //verifies color entered works
 {
-    for(i = 1; i <= 100; i++)
+    let test = new Option().style;
+    test.color = strColor;
+
+    if(test.color == strColor)
     {
-        const oneHundredDiv = document.getElementById("oneHundred");
-        let numberSpan = document.createElement("span");
-        const breakLine = document.createElement("br");
-
-        if(i % 3 == 0 && i % 5 == 0) //multiple of 3 and 5
-        {
-            numberSpan.innerText = "FizzBuzz,";
-        }
-        else if(i % 3 == 0 || i % 5 == 0) //multiple of 3 or 5
-        {
-            if(i % 3 == 0)
-            {
-                numberSpan.innerText = "Fizz,";
-            }
-            else            //multiple of 5
-            {
-                if(i == 100) //End 100th number with a period
-                {
-                    numberSpan.innerText = "Buzz.";
-                }
-                else
-                {
-                    numberSpan.innerText = "Buzz,";
-                }
-            }
-        }
-        else       //not a multiple of 3 or 5
-        {
-            numberSpan.innerText = `${i},`;
-        }
-
-        oneHundredDiv.appendChild(numberSpan);
-
-        if(i % 10 == 0)    // break line after every 10 numbers
-        {
-            oneHundredDiv.appendChild(breakLine);
-        }
-    
+        return true;
+    }
+    else
+    {
+        return false;
     }
 }
 
-printOneHundred();
+let favColor = prompt("Favorite Color? [i.e: red, blue, salmon...]");
+favColor = favColor.toLowerCase();
+
+while(!isValidColor(favColor))
+{
+    favColor = prompt("Not valid Color! Try again.");
+    favColor = favColor.toLowerCase();
+}
+
+sentencesP.style.backgroundColor = favColor;
