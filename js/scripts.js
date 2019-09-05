08716ea3985e8786563ccbc3453f14b90061bb12
mikeosborne90 <mikeosborne1990@gmail.com>
2019-09-05 11:28:58 -0700

    Basic Functionality for form take home challenege, still need to add submit button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,101 +1,84 @@
 const body = document.body;
 const container = document.createElement("div");
-
 container.className = "container text-center";
-
-let test = body.appendChild(container);
-
-console.log("my html: ", test);
 
 const row = document.createElement("main");
 row.className = "row";
 
 const content = document.createElement("div");
-
 content.className = "col-md-6 col-md-offset-2 content";
 
 row.appendChild(content);
 container.appendChild(row);
 body.appendChild(container);
 
-// form tag > input:email:t-email - input:subject:t-text - textarea:t-text - button:t-submit
-
-const form = document.createElement("form");
-
-form.method = "GET";
-form.action = "js/form-submission.js";
-form.name = "myForm";
-
-const inputEmail = document.createElement("input");
-
-inputEmail.name = "email";
-inputEmail.type = "email";
-inputEmail.className = "form-control";
-inputEmail.placeholder = "Enter email here";
-
-const inputSubject = document.createElement("input");
-
-inputSubject.name = "subject";
-inputSubject.type = "text";
-inputSubject.className = "form-control";
-
-const textArea = document.createElement("textarea");
-
-textArea.name = "textArea";
-textArea.type = "text";
-textArea.className = "form-control";
-
-const button = document.createElement("button");
-
-button.name = "button";
-button.type = "submit";
-
-form.appendChild(inputEmail);
-form.appendChild(inputSubject);
-form.appendChild(textArea);
-form.appendChild(button);
-
-content.appendChild(form);
-
-// function globalFormElements(element, elementType, elementName, elementClass, elementPlaceholder, elementTextArea = 0, elementId)
-// {
-//     const newElement = document.createElement(element);
-    
-//     newElement.setAttribute("type",elementType);
-//     newElement.className = elementClass;
-//     newElement.name = elementName;
-//     newElement.placeholder = elementPlaceholder;
-//     newElement.textArea = elementTextArea;
-//     newElement.id = elementId;
-
-//     return newElement;
-// }
-
-// console.log(globalFormElements("input", "text", "example", "form-control bg-primary", "Type your example here"));
-
-// console.log(globalFormElements("textarea", "text", "message", "bg-danger", "Please let us know your concerns here."));
-
-const textAreaElement = (name, placeHolder, elementClass, cols = 4, rows = 2, elementId) => 
-{
-    const element = document.createElement("textarea");
-    element.setAttribute("type", "text");
-    element.className = elementClass;
-    element.setAttribute("name", name);
-    element.setAttribute("placeholder", placeHolder);
-    element.setAttribute("cols", cols);
-    element.setAttribute("rows", rows);
-    element.id = elementId;
-
-    return element;
-}
-
-// console.log(gloabalFormElements("input", "text", "example", ))
-
-const labelGenerator = (forElement, text) => 
+function createLabel(forValue, textValue)
 {
     const label = document.createElement("label");
-    label.setAttribute("for", forElement);
-    label.innerText = text;
+
+    label.setAttribute("for", forValue);
+    label.innerText = textValue;
 
     return label;
 }
+
+// (param = 0) should be optional parameter
+function createInput(typeValue, nameValue, placeholderValue, classValue, idValue)
+{
+    const input = document.createElement("input");
+
+    input.type = typeValue;
+    input.name = nameValue;
+    input.placeholder = placeholderValue;
+    input.className = classValue;
+    input.id = idValue;
+
+    return input;
+}
+
+//receives array of labels and array of inputs
+function createForm(name, method, action, labels, inputs)
+{
+    const form = document.createElement("form");
+
+    form.name = name;
+    form.method = method;
+    form.action = action;
+
+    if(labels.length == inputs.length) // equal # of labels and inputs
+    {
+        for(i = 0; i < labels.length; i++)
+        {
+            form.appendChild(labels[i]);
+            form.appendChild(inputs[i]);
+        }
+    }
+    else
+    {
+        console.log("Form # of labels and inputs do not match!");
+    }
+
+    return form;
+}
+
+let myLabels = [];
+let label1 = createLabel("email", "Email");
+let label2 = createLabel("subject", "Subject");
+let label3 = createLabel("message", "Message");
+
+myLabels.push(label1);
+myLabels.push(label2);
+myLabels.push(label3);
+
+let myInputs = [];
+let input1 = createInput("email", "email", "Enter Email Here","form-control", "email");
+let input2 = createInput("text", "subject", "Enter Subject Here","form-control", "subject");
+let input3 = createInput("text", "message", "Enter Message Here","form-control", "message");
+
+myInputs.push(input1);
+myInputs.push(input2);
+myInputs.push(input3);
+
+let myForm = createForm("basicForm", "GET", "js/form-submission.js", myLabels, myInputs);
+
+content.appendChild(myForm);
